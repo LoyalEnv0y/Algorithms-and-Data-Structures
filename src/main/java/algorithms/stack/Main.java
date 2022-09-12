45070a838ad9706780fa00134827944d73d1d9e9
Çetin <cetin200012@gmail.com>
2022-09-12 13:41:09 +0300

    Implemented the Iterable interface for Stack class

--- src/main/java/algorithms/stack/Main.java
+++ src/main/java/algorithms/stack/Main.java
@@ -12,10 +12,23 @@ public class Main {
         // Fill the stack, print it and check it's size and capacity
         for (int i = 1; i <= 8; i++) {
             myStack.push(i);
+
+            // Check if the iterator works correctly after each push
+            for (int s : myStack) {
+                System.out.print(s + ", ");
+            }
+            System.out.println();
         }
         printStack(myStack);
         printSizeAndCap(myStack);
         System.out.println();
+
+        // Use the foreach loop that is available because of the iterable and iterator interfaces
+        for (int i : myStack) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+
 
         // Pop the last Non-Null item in the stack
         System.out.println("Removed item: " + myStack.pop());
@@ -26,6 +39,12 @@ public class Main {
         // Remove items until the size of the array is 25% of capacity
         while (myStack.size() > 2) {
             System.out.println("Removed item: " + myStack.pop());
+
+            // Check if the iterator works correctly after each pop
+            for (int i : myStack) {
+                System.out.print(i + ", ");
+            }
+            System.out.println();
         }
 
         printStack(myStack);
--- src/main/java/algorithms/stack/Stack.java
+++ src/main/java/algorithms/stack/Stack.java
@@ -1,6 +1,7 @@
 package algorithms.stack;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * <p>Stacks are LIFO (Last in first out) meaning that you process the
@@ -8,7 +9,7 @@ import java.util.Arrays;
  *
  * <p>Push an item to the end of the stack and pop from the end of the stack.
  */
-public class Stack<T> {
+public class Stack<T> implements Iterable<T> {
     private T[] stack;
     private int lastIndex;
 
@@ -145,5 +146,50 @@ public class Stack<T> {
     public String toString() {
         T[] nonNullItems = Arrays.copyOfRange(this.stack, 0, lastIndex + 1);
         return Arrays.toString(nonNullItems);
+    }
+
+    /**
+     * <p>To satisfy the {@code Iterable} interface, {@code iterator}
+     * method needs to be implemented. Method is called when client tries
+     * to iterate over the stack with a {@code foreach} loop. It then
+     * returns a {@code StackIterator} class to be used by the for loop.
+     *
+     * @return a new instance of {@code StackIterator} class.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new StackIterator();
+    }
+
+    private class StackIterator implements Iterator<T> {
+        private int index = 0;
+
+        /**
+         * <p>Checks if the current item in the {@code index} exists.
+         * It is being called at the beginning of every iteration in a
+         * {@code foreach} loop to check if the loop reached the end.
+         *
+         * @return true if the stack <i>is NOT</i> empty <i>AND</i>
+         * index is less than or equal to the {@code lastIndex}.
+         */
+        @Override
+        public boolean hasNext() {
+            return !isEmpty() && index <= lastIndex;
+        }
+
+        /**
+         * <p>Returns the element at the {@code index} in the stack.
+         * Increments the index by one. It is called when the inside
+         * of the {@code foreach} loop is finished executing and the
+         * loop needs to be advancing.
+         *
+         * @return the element at the {@code index} in the stack.
+         */
+        @Override
+        public T next() {
+            T toReturn = stack[index];
+            index++;
+            return toReturn;
+        }
     }
 }
