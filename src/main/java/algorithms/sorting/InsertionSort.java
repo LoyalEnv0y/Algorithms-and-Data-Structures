b16d2b6edf391f18630916533d3f44ef32964b80
Çetin <cetin200012@gmail.com>
2022-09-17 12:55:54 +0300

    refactored Insertion Sort

--- src/main/java/algorithms/sorting/InsertionSort.java
+++ src/main/java/algorithms/sorting/InsertionSort.java
@@ -2,8 +2,7 @@ package algorithms.sorting;
 
 public class InsertionSort implements Sorting {
     public void sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-
+        for (int i = 1; i < arr.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     exchange(arr, j, j - 1);
--- src/main/java/algorithms/sorting/Main.java
+++ src/main/java/algorithms/sorting/Main.java
@@ -1,34 +1,35 @@
 package algorithms.sorting;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[100000];
+        int[] arr = new int[10];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
 
-        testSorting(new SelectionSort(), arr);
+//        testSorting(new SelectionSort(), arr);
         testSorting(new InsertionSort(), arr);
-        testSorting(new ShellSort(), arr);
+/*        testSorting(new ShellSort(), arr);
         testSorting(new MergeSort(), arr);
-        testSorting(new QuickSort(), arr);
+        testSorting(new QuickSort(), arr);*/
     }
 
     public static void testSorting(Sorting sortingType, int[] arr) {
         System.out.println(sortingType.getClass().getSimpleName() + ": ");
 
         shuffleArray(arr);
-        //System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
 
         long startTime = System.nanoTime();
         sortingType.sort(arr);
         long endTime = System.nanoTime();
 
         System.out.println(sortingType.isSorted(arr));
-        //System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
         System.out.println((endTime - startTime) / Math.pow(10, 9));
 
         System.out.println();
