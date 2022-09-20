bdad8f72cedbe88955f8cb8a8887199871885449
Çetin <cetin200012@gmail.com>
2022-09-20 14:19:12 +0300

    implemented the Binary Search Tree using recursion

--- src/main/java/algorithms/Tree/Main.java
+++ src/main/java/algorithms/Tree/Main.java
@@ -5,11 +5,25 @@ public class Main {
         BinarySearchTree<Integer> bts = new BinarySearchTree<>();
         bts.insert(5);
         bts.insert(1);
-        bts.insert(7);
         bts.insert(2);
         bts.insert(8);
+        bts.insert(9);
         bts.insert(6);
 
         System.out.println(bts.search(2));
+
+        System.out.println(bts.getMin());
+        System.out.println(bts.getMax());
+        System.out.println();
+
+        bts.traverse("in");
+        System.out.println();
+        bts.traverse("pre");
+        System.out.println();
+        bts.traverse("post");
+        System.out.println();
+
+        bts.remove(5);
+        bts.traverse("in");
     }
 }
